325da389b3071d01c2d9149af297afdbd410fac5
Midax2 <dmytro.dzhusov@gmail.com>
2024-04-08 12:26:53 +0200

    Processes have priorytet now

--- Lab4/TestProcesyWin.c
+++ Lab4/TestProcesyWin.c
@@ -6,14 +6,16 @@
 typedef struct {
     DWORD pid;
     HANDLE handle;
+    int priority;
 } ProcessInfo;
 
 ProcessInfo processes[MAX_PROCESSES];
 int num_processes = 0;
 
-void addProcess(DWORD pid, HANDLE handle) {
+void addProcess(DWORD pid, HANDLE handle, int priority) {
     processes[num_processes].pid = pid;
     processes[num_processes].handle = handle;
+    processes[num_processes].priority = priority;
     num_processes++;
 }
 
@@ -37,29 +39,26 @@ void stopProcess(int processIndex) {
     num_processes--;
 }
 
-
 int main() {
     printf("ProgramGlowny - Start\n");
 
     while (1) {
         for (int i = 0; i < num_processes; i++) {
-            for (int i = 0; i < num_processes; i++) {
-                DWORD exitCode;
-                if (!GetExitCodeProcess(processes[i].handle, &exitCode)) {
-                    printf("Failed to get exit code for process %d. Error code: %d\n", processes[i].pid, GetLastError());
-                    continue;
+            DWORD exitCode;
+            if (!GetExitCodeProcess(processes[i].handle, &exitCode)) {
+                printf("Failed to get exit code for process %d. Error code: %d\n", processes[i].pid, GetLastError());
+                continue;
+            }
+            if (exitCode != STILL_ACTIVE) {
+                CloseHandle(processes[i].handle);
+                for (int j = i; j < num_processes - 1; j++) {
+                    processes[j] = processes[j + 1];
                 }
-                if (exitCode != STILL_ACTIVE) {
-                    // Proces jest już zakończony, usuwamy go z tablicy
-                    CloseHandle(processes[i].handle);
-                    for (int j = i; j < num_processes - 1; j++) {
-                        processes[j] = processes[j + 1];
-                    }
-                    num_processes--;
-                    i--; // Spowoduje to ponowne sprawdzenie procesu na tej samej pozycji
-                }
+                num_processes--;
+                i--; 
             }
         }
+
         char command[50];
         printf("Enter command (1: Create process, 2: Stop process, 3: List processes, 4: Exit): ");
         fgets(command, sizeof(command), stdin);
@@ -67,44 +66,74 @@ int main() {
         int choice = atoi(command);
 
         switch (choice) {
-        case 1: {
-            char exe_path[] = "ProgramPodrzedny.exe";
-            STARTUPINFO structStartupInfo;
-            PROCESS_INFORMATION structProcInfo;
+            case 1: {
+                char exe_path[] = "ProgramPodrzedny.exe";
+                STARTUPINFO structStartupInfo;
+                PROCESS_INFORMATION structProcInfo;
 
-            GetStartupInfo(&structStartupInfo);
+                GetStartupInfo(&structStartupInfo);
 
-            if (!CreateProcess(NULL, exe_path, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &structStartupInfo, &structProcInfo)) {
-                printf("CreateProcess failed (%d).\n", GetLastError());
+                printf("Enter priority for the new process (1-4): ");
+                fgets(command, sizeof(command), stdin);
+                int priority = atoi(command);
+
+                if (priority < 1 || priority > 4) {
+                    printf("Invalid priority. Please enter a value between 1 and 4.\n");
+                    break;
+                }
+
+                DWORD priorityClass;
+                switch (priority) {
+                    case 1:
+                        priorityClass = IDLE_PRIORITY_CLASS;
+                        break;
+                    case 2:
+                        priorityClass = BELOW_NORMAL_PRIORITY_CLASS;
+                        break;
+                    case 3:
+                        priorityClass = NORMAL_PRIORITY_CLASS;
+                        break;
+                    case 4:
+                        priorityClass = ABOVE_NORMAL_PRIORITY_CLASS;
+                        break;
+                }
+
+                if (!CreateProcess(NULL, exe_path, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &structStartupInfo, &structProcInfo)) {
+                    printf("CreateProcess failed (%d).\n", GetLastError());
+                    break;
+                }
+
+                if (!SetPriorityClass(structProcInfo.hProcess, priorityClass)) {
+                    printf("Failed to set priority class for the process. Error code: %d\n", GetLastError());
+                    break;
+                }
+
+                addProcess(structProcInfo.dwProcessId, structProcInfo.hProcess, priority);
                 break;
             }
-
-            addProcess(structProcInfo.dwProcessId, structProcInfo.hProcess);
-            break;
-        }
-        case 2: {
-            printf("Enter the index of the process to stop: ");
-            fgets(command, sizeof(command), stdin);
-            int processIndex = atoi(command) - 1;
-            stopProcess(processIndex);
-            break;
-        }
-        case 3: {
-            printf("List of running processes:\n");
-            for (int i = 0; i < num_processes; i++) {
-                printf("%d. PID: %d\n", i + 1, processes[i].pid);
+            case 2: {
+                printf("Enter the index of the process to stop: ");
+                fgets(command, sizeof(command), stdin);
+                int processIndex = atoi(command) - 1;
+                stopProcess(processIndex);
+                break;
             }
-            break;
-        }
-        case 4: {
-            for (int i = 0; i < num_processes; i++) {
-                stopProcess(i);
+            case 3: {
+                printf("List of running processes:\n");
+                for (int i = 0; i < num_processes; i++) {
+                    printf("%d. PID: %d, Priority: %d\n", i + 1, processes[i].pid, processes[i].priority);
+                }
+                break;
             }
-            printf("ProgramGlowny - Finish\n");
-            return 0;
-        }
-        default:
-            printf("Invalid command.\n");
+            case 4: {
+                for (int i = 0; i < num_processes; i++) {
+                    stopProcess(i);
+                }
+                printf("ProgramGlowny - Finish\n");
+                return 0;
+            }
+            default:
+                printf("Invalid command.\n");
         }
     }
 
